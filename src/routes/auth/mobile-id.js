360ea2c440fe6420f3bc44c78839bc4269686b23
Argo Roots <argo.roots@adm.ee>
2021-08-30 00:33:48 +0300

    Log session

--- src/routes/auth/mobile-id.js
+++ src/routes/auth/mobile-id.js
@@ -100,6 +100,8 @@ router.post('/:key', function(req, res, next) {
             entu.getMobileIdSession(req.params.key, callback)
         },
         function (session, callback) {
+            console.log(session);
+
             axios.get('https://mid.sk.ee/mid-api/authentication/session/' + session.sessionID)
             .then((response) => {
                 callback(null, response.data)
